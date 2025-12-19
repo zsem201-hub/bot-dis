b7e0b341193b5971707a77e6255a02f172d5449f
zsem201-hub <zsem201@gmail.com>
2025-12-19 10:54:39 +0700

    Update __init__.py

--- obfuscator/__init__.py
+++ obfuscator/__init__.py
@@ -1,1 +1,15 @@
-from .core import LuaShieldObfuscator
+from .core import LuaShieldPro
+from .virtualization import VirtualMachine
+from .control_flow import ControlFlowObfuscator
+from .encryption import EncryptionEngine
+from .anti_tamper import AntiTamperGenerator
+from .junk_code import JunkCodeGenerator
+
+__all__ = [
+    'LuaShieldPro',
+    'VirtualMachine', 
+    'ControlFlowObfuscator',
+    'EncryptionEngine',
+    'AntiTamperGenerator',
+    'JunkCodeGenerator'
+]
